cc4790e5d547314209937acf198c6826869082a6
matt-derocher-simplisafe <matt.derocher@simplisafe.com>
2026-03-26 11:04:09 -0400

    Added settings for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyReactRouter(),
+  ],
 });
